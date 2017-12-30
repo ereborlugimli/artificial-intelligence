9adc0a4a15a0544721597ea98c90dbd5a7ce8f12
ereborlugimli <yenidoganbusra@gmail.com>
2017-12-31 00:15:32 +0300

    Update knn.go

--- knn.go
+++ knn.go
@@ -3,9 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	//"io/ioutil"
 	"math"
 	"os"
-	//s"reflect"
 	"sort"
 	"strconv"
 	"strings"
@@ -13,11 +13,14 @@ import (
 
 func main() {
 
-	var name string
-	var age, income, k, total, r1, r2, r3, near, counterA, counterB int
+	var name, class, text string
+	var age, income, k, t, total, r1, r2, r3, near, counterA, counterB int
 	var a []int
 	m := make(map[int]string)
 	m2 := make(map[int]string)
+	counterA = 0
+	counterB = 0
+	t = 0
 
 	fmt.Println("Enter the customer's name : ")
 	fmt.Scanf("%s", &name)
@@ -67,18 +70,36 @@ func main() {
 			m2[k] = string(s)
 		}
 	}
-	counterA = 0
-	counterB = 0
-	for t := 0; t < near; t++ {
-		for _, k := range m2 {
-			if m2[k] == "A" {
-				counterA++
-			} else if m2[k] == "B" {
-				counterB++
+	//fmt.Println(m2)
+	for _, s := range m2 {
+		if t < 3 {
+			if s == "A" {
+				counterA = counterA + 1
+			} else if s == "B" {
+				counterB = counterB + 1
 			}
-
 		}
+		t++
 	}
-	fmt.Println(m2)
-	fmt.Println(counterA, counterB)
+
+	if counterA > counterB {
+		class = "A"
+		fmt.Println("A sınıfına ait")
+	} else {
+		class = "B"
+		fmt.Println("B sınıfına ait")
+	}
+	text = name + "," + strconv.Itoa(age) + "," + strconv.Itoa(income) + "," + strconv.Itoa(k) + "," + class + "\n"
+	x, err := os.OpenFile("knnData.txt", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer x.Close()
+
+	if _, err = x.WriteString(text); err != nil {
+		panic(err)
+	}
+
 }
+
